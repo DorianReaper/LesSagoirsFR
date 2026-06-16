8de6c8c6a99773ce2f2b4f3cdb3f44f0b6d28b31
DorianReaper <dorianreaper928@gmail.com>
2026-06-17 00:24:10 +0200

    Delete quiz.js

--- quiz.js
+++
@@ -1,6 +0,0 @@
-// ==================== QUIZ CLASSIQUE ====================
-
-function openQuizNewTab() {
-    // Ouvre le fichier quiz.html dans un nouvel onglet
-    window.open('quiz.html', '_blank');
-}
