7a8cbfebcf647990c93d996cd0d86ca583b58199
DorianReaper <dorianreaper928@gmail.com>
2026-06-17 00:24:00 +0200

    Delete map.js

--- map.js
+++
@@ -1,6 +0,0 @@
-// ==================== QUIZ ALÉATOIRE ====================
-
-function openRandomQuiz() {
-    // Ouvre le fichier random.html dans un nouvel onglet
-    window.open('random.html', '_blank');
-}
