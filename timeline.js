9d5db93e1bd2cd4d35d671a4e1c5361fbebaa36c
DorianReaper <dorianreaper928@gmail.com>
2026-06-16 21:12:15 +0200

    TKT

---
+++ timeline.js
@@ -0,0 +1,6 @@
+// ==================== FRISE CHRONOLOGIQUE ====================
+
+function openTimelineNewTab() {
+    // Ouvre le fichier timeline.html dans un nouvel onglet
+    window.open('timeline.html', '_blank');
+}
