ffea77882691a2122f1bd31d336d1002834fb8a5
DorianReaper <dorianreaper928@gmail.com>
2026-06-17 00:22:55 +0200

    Delete timeline.js

--- timeline.js
+++
@@ -1,6 +0,0 @@
-// ==================== FRISE CHRONOLOGIQUE ====================
-
-function openTimelineNewTab() {
-    // Ouvre le fichier timeline.html dans un nouvel onglet
-    window.open('timeline.html', '_blank');
-}
